885042b93e17dcd13523e99ace27aea322b9a9d1
Göbölyös Dávid <davidgobolyos@gmail.com>
2020-08-18 20:41:10 +0200

    routing for new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { SeasonPageComponent } from './pages/season-page/season-page.component';
+import { CircuitsPageComponent } from './pages/circuits-page/circuits-page.component';
+import { DriversPageComponent } from './pages/drivers-page/drivers-page.component';
+import { TeamsPageComponent } from './pages/teams-page/teams-page.component';
 
 const routes: Routes = [
   { path: 'seasons', component: SeasonPageComponent },
+  { path: 'teams', component: TeamsPageComponent },
+  { path: 'drivers', component: DriversPageComponent },
+  { path: 'circuits', component: CircuitsPageComponent },
   { path: '', component: MainPageComponent },
-
 ];
 
 @NgModule({
